576fefbefc5b960c49900a30b021dcf74501f71e
Imoldev <imoldev@yandex.ru>
2020-05-23 19:18:10 +0400

    third lesson stub (заглушка)

--- Students/molochnikov/src/components/Message.jsx
+++ Students/molochnikov/src/components/Message.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 export default class Message extends React.Component {
 
-    render () {
+    render() {
         return (
-        <li className="superchat__message-item">
-            <div className="superchat__message-author">{this.props.author}</div>
-            <div className="superchat__message-text"><pre>{this.props.text}</pre></div>
-        </li>
+            <li className="superchat__message-item">
+                <div className="superchat__message-author">{this.props.author}</div>
+                <div className="superchat__message-text">
+                    <pre>{this.props.text}</pre>
+                </div>
+            </li>
         );
     }
 
--- Students/molochnikov/src/index.jsx
+++ Students/molochnikov/src/index.jsx
@@ -1,14 +1,38 @@
 
+
+import {createStore} from 'redux';
+
+function playList (state=[], action) {
+    console.log(action);
+    return state;
+}
+
+const store = createStore(playList);
+
+store.subscribe(()=>{
+    console.log('subs', store.getState());
+});
+
+store.dispatch({type: 'TYPE', payload: 'данные'});
+
+/*
 import './components/sass/messages.sass';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import MessagesRepository from  './components/MessagesRepository.jsx';
+import MessageField from  './components/MessageField.jsx';
+
+// redux
+import {Proveder} from 'react-redux';
+import initStore from './store/store.js';
+
 
 
 ReactDOM.render(
-    <MessagesRepository />,
+   <Provider store={initStore}>
+       <MessageField/>
+   </Provider>,
     document.getElementById('root'),
  );
- 
+ */
